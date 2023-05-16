aaa810a4b48c6801a38a7001ba2e1d33dfceeab8
Morozova Anastasia <Anafrost1993@gmail.com>
2023-05-16 23:22:05 +0700

    feat: added footer

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import "./styles.css";
 import "./index.css";
 
 import { Header } from "./components/Header";
+import { Footer } from "./components/Footer";
 
 export const App = () => {
   return (
     <div className="App">
       <Header />
+      <Footer />
     </div>
   );
 };
